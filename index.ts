e5fecac57aa8cc909fe652467d34b244e6f2b647
Victor Kadiata <vickadiata@gmail.com>
2025-02-18 19:55:04 +0100

    Ajout de la fonction getFirstElement

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 //Écrivez une fonction add qui prend deux nombres en entrée et retourne leur somme.
-function add(a :number, b :number) : number {
+function add(a: number, b: number): number {
     return a + b;
 }
 
-console.log(add(5, 3)); 
+console.log(add(5, 3));
 console.log(add(-2, 7));
 
 
@@ -16,8 +16,13 @@ function isEven(n: number): boolean {
 console.log(isEven(2)); // true
 console.log(isEven(3)); // false
 
-function toUpperCaseString (input:string):string {
-
-    return input.toUpperCase()
-
+function toUpperCaseString(input: string): string {
+    return input.toUpperCase();
 }
+
+// Écrivez une fonction getFirstElement qui retourne le premier élément d'un tableau.
+function getFirstElement<T>(arr: T[]): T {
+    return arr[0];
+}
+
+console.log(getFirstElement([1, 2, 3])); // 1
